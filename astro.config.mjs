a95c1efd40f1afcb8e757e897875f348fe4733a8
Alejandro <alejandro@asr-oss.com>
2025-01-08 07:30:41 +0100

    avances seccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+import tailwind from '@astrojs/tailwind';
+
+import auth from 'auth-astro';
+
+import db from '@astrojs/db';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: netlify(),
+  integrations: [tailwind(), auth(), db(), react()]
+});
---
+++ db/seed.ts
@@ -0,0 +1,61 @@
+import { db, Role, User, Product, ProductImage } from 'astro:db';
+import { v4 as UUID } from 'uuid';
+import bcrypt from 'bcryptjs';
+import { seedProducts } from './seed-data';
+
+// https://astro.build/db/seed
+export default async function seed() {
+  const roles = [
+    { id: 'admin', name: 'Administrator' },
+    { id: 'user', name: 'User' },
+    { id: 'super-user', name: 'Super User' },
+  ];
+
+  const jonhnDoe = {
+    id: UUID(),
+    name: 'John Doe',
+    email: 'john.doe@google.com',
+    password: bcrypt.hashSync('123456'),
+    role: 'admin',
+  };
+  const janeDoe = {
+    id: UUID(),
+    name: 'Jane Doe',
+    email: 'jane.doe@google.com',
+    password: bcrypt.hashSync('123456'),
+    role: 'user',
+  };
+
+  await db.insert(Role).values(roles);
+  await db.insert(User).values([jonhnDoe, janeDoe]);
+
+  const queries: any = [];
+
+  seedProducts.forEach((p) => {
+    const product = {
+      id: UUID(),
+      description: p.description,
+      stock: p.stock,
+      price: p.price,
+      sizes: p.sizes.join(','),
+      slug: p.slug,
+      tags: p.tags.join(','),
+      title: p.title,
+      type: p.type,
+      gender: p.gender,
+      user: jonhnDoe.id,
+    };
+
+    queries.push(db.insert(Product).values(product));
+
+    p.images.forEach((img) => {
+      const image = {
+        id: UUID(),
+        image: img,
+        productId: product.id,
+      };
+      queries.push(db.insert(ProductImage).values(image));
+    });
+  });
+  await db.batch(queries);
+}
